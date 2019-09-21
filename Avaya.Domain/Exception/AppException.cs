da045346891a4479754024e917b2c2902fb975ff
Phuoc <manhphuoc1995@gmail.com>
2019-09-21 20:27:22 +0700

    Add Swagger and Logging

---
+++ Avaya.Domain/Exception/AppException.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Ex
+
+//namespace Avaya.Domain.Exception
+//{
+//    public class AppException : Exception
+//    {
+//        public AppException()
+//        {
+//        }
+//        public AppException(string message)
+//            : base(message)
+//        {
+//        }
+
+//        public AppException(string messageFormat, params object[] args)
+//            : base(string.Format(messageFormat, args))
+//        {
+//        }
+
+//        public AppException(string message, Exception innerException)
+//            : base(message, innerException)
+//        {
+//        }
+
+//        protected AppException(SerializationInfo info, StreamingContext context)
+//            : base(info, context)
+//        {
+//        }
+//    }
+//}
