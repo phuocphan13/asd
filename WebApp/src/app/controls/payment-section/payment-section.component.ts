ac73dd8772260fd122caa047cf9fefc920dd749c
NGUYỄN ĐÌNH DUY TÀI <tai.nguyendinhduy@gmail.com>
2019-09-22 11:53:57 +0700

    Final payment UI

--- WebApp/src/app/controls/payment-section/payment-section.component.ts
+++ WebApp/src/app/controls/payment-section/payment-section.component.ts
@@ -12,12 +12,18 @@ export class PaymentSectionComponent implements OnInit {
   img: string = 'https://revengeofthefans.com/wp-content/uploads/2019/01/Breaking-Bad-RTF-e1548338096892.jpg';
   moviename: string = 'Cranky old man';
   theater: string = 'CGV ';
-  // showtime: object kiểu Date
+  showtime:string ='09:30';
+  showdate:string ='22/07/2019'
   seatnumber: string = '13F';
   constructor() { }
 
   ngOnInit() {
     this.completePrice=0;
+  }
+  
+  checkOut()
+  {
+    alert("ready to use!!");
   }
 
 }
