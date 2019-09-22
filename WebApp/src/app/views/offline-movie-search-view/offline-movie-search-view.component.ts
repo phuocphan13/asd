9d3f94ecabf57824f51a65cfe9d0fb59207cf5ca
Phuoc <manhphuoc1995@gmail.com>
2019-09-22 11:41:10 +0700

    Code Css

--- WebApp/src/app/views/offline-movie-search-view/offline-movie-search-view.component.ts
+++ WebApp/src/app/views/offline-movie-search-view/offline-movie-search-view.component.ts
@@ -29,7 +29,7 @@ export class OfflineMovieSearchViewComponent implements OnInit {
       name: "Ngôi Nhà Bươm Bướm",
       time: "105 phút",
       decription: "“Ngôi Nhà Bươm Bướm” là bộ phim xoay quanh câu chuyện nên duyên của Hoàng (Liên Bỉnh Phát) và Mai (Hoàng Yến Chibi). Bằng sự khôn khéo và lém lĩnh, Mai đã thuyết phục được bố mẹ từ Bắc vào Nam để gặp gỡ và bàn chuyện cưới hỏi của cả hai. Mọi chuyện tưởng chừng sẽ rất êm đẹp cho đến khi cả hai bên gia đình đều bị phơi bày những bí mật “động trời” cùng sự khác biệt về mọi mặt. Điều gì sẽ cứu vãn được tình yêu nhiệt thành của tuổi trẻ và trao cho họ một cái kết đầy viên mãn?",
-      showtime: "11:30 13:25 15:45 16:35",
+      showtime: ["11:30", "13:25", "15:45", "16:35"],
     },
     {
       id: 1,
@@ -38,7 +38,7 @@ export class OfflineMovieSearchViewComponent implements OnInit {
       name: "BẠN HỌC CÕI ÂM",
       time: "93 phút",
       decription: "Những ngày đầu tiên tại trường trung học của cậu nam sinh Alex chẳng khác gì địa ngục, khi nạn bắt nạt và bạo lực học đường diễn ra như cơm bữa. Sự xuất hiện của cô bạn Maggie khiến cho cuộc sống của Alex thú vị hơn hẳn. Cả hai sớm trở thành đôi bạn thân. Tuy nhiên, những sự việc kì lạ và kinh hoàng bắt đầu xảy ra. Đã có những cái chết được gây ra bởi những “bạn học cõi âm” khiến cho ngôi trường trở nên đáng sợ hơn bao giờ hết.",
-      showtime: "11:30 13:25 15:45 16:35",
+      showtime: ["11:30", "13:25", "15:45", "16:35"],
     },];
   }
 
