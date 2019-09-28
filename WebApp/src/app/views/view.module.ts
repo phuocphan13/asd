b0679767d10517daf2efd0619db5fe9ed1d292f3
Phuoc <manhphuoc1995@gmail.com>
2019-09-28 11:57:22 +0700

    Commit

--- WebApp/src/app/views/view.module.ts
+++ WebApp/src/app/views/view.module.ts
@@ -5,6 +5,7 @@ import { SidenewsSectionComponent } from './side-news/side-news.component';
 import { OfflineMovieSearchViewModule } from './offline-movie-search-view/offline-movie-search-view.module';
 import { PaymentViewModule } from './payment-view/payment-view.module';
 import { FilmOnlineViewModule } from './film-online-view/film-online-view.module';
+import { ControlModule } from '../controls/control.module';
 
 @NgModule({
   declarations: [
