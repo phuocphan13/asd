4f7a77a21fee73e9784b501a7052e07e55ee8d20
Phuoc <manhphuoc1995@gmail.com>
2019-09-28 09:44:15 +0700

    Refactor File Structure

--- WebApp/src/app/controls/control.module.ts
+++ WebApp/src/app/controls/control.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingTicketComponent } from './booking-ticket/booking-ticket.component';
-import { TimeMovieCinemaComponent } from './time-movie-cinema/time-movie-cinema.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,17 +9,13 @@ import { PaymentSectionComponent } from './payment-section/payment-section.compo
 @NgModule({
   declarations: [
     BookingTicketComponent,
-    TimeMovieCinemaComponent,
     PaymentSectionComponent,
   ],
   imports: [
     CommonModule,
-    NgSelectModule,
     BrowserAnimationsModule,
-    BsDatepickerModule.forRoot(),
   ],
   exports: [
-    TimeMovieCinemaComponent,
     BookingTicketComponent,
     PaymentSectionComponent
   ],
