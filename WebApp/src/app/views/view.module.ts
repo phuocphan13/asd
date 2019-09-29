9a9caab0aa1fabb3cdcef83d419d045c4611d9aa
Phuoc <manhphuoc1995@gmail.com>
2019-09-29 12:06:39 +0700

    Commit

--- WebApp/src/app/views/view.module.ts
+++ WebApp/src/app/views/view.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsViewComponent } from './news-view/news-view.component';
-import { ControlModule } from '../controls/control.module';
 import { MovieNewsViewModule } from './movie-news-view/movie-news-view.module';
-import { SidenewsSectionComponent } from './side-news/side-news.component';
 import { OfflineMovieSearchViewModule } from './offline-movie-search-view/offline-movie-search-view.module';
 import { PaymentViewModule } from './payment-view/payment-view.module';
 import { FilmOnlineViewModule } from './film-online-view/film-online-view.module';
