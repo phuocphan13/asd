d6ca7b1179a9538cddd63c1a811c43eac1ddc986
kimocheese.kimochi@gmail.com <thuanphat0611@gmail.com>
2019-10-06 09:30:56 +0700

    fix bug

--- Avaya.Service/PaymentService/PaymentService.cs
+++ Avaya.Service/PaymentService/PaymentService.cs
@@ -21,11 +21,20 @@ namespace Avaya.Service.PaymentService
         public List<PaymentModel> GetListPayment(List<SearchServiceModel> searchService)
         {
             var listpayment = new List<PaymentModel>();
+
             foreach (var item in searchService)
             {
-                var abc = _serviceRepository.FirstOrDefault(x => x.Id == item.Id).MapTo<PaymentModel>();
-                listpayment.Add(abc);
+                var list = _serviceRepository.FirstOrDefault(x => x.Id == item.Id).MapTo<PaymentModel>();
+                if (list == null)
+                    return null;
+
+                list.Total = item.Quantity * list.Price;
+
+                list.Quantity = item.Quantity;
+
+                listpayment.Add(list);
             }
+
             return listpayment;
         }
     }
