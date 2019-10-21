7c4fa665cd454134ecc51bcfcf12f633aaad3cfc
kimocheese.kimochi@gmail.com <thuanphat0611@gmail.com>
2019-10-21 20:20:31 +0700

    merge seatservice

--- Avaya.API/Controllers/SeatController.cs
+++ Avaya.API/Controllers/SeatController.cs
@@ -6,28 +6,34 @@ using System.Web.Http.Cors;
 using Avaya.Core.Repositories;
 using Avaya.Domain.Models;
 using Avaya.Model;
+using Avaya.Model.SaveSeat;
 using Avaya.Model.ShowSeat;
 using Avaya.Service;
-using Avaya.Service.ShowSeatService;
+using Avaya.Service.SeatService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Avaya.API.Controllers
 {
     [Route("api/[controller]/[action]")]
     [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
-    public class ShowSeatController : Controller
+    public class SeatController : Controller
     {
-        private readonly IShowSeatService _showSeatService;
-
-        public ShowSeatController(IShowSeatService showSeatService)
+        private readonly ISeatService _SeatService;
+        public SeatController(ISeatService SeatService)
         {
-            _showSeatService = showSeatService;
+            _SeatService = SeatService;
         }
 
         [HttpPost]
         public IActionResult GetListSeats([FromBody]SearchSeatModel searchSeat)
         {
-            var result = _showSeatService.GetShowSeat(searchSeat);
+            var result = _SeatService.GetShowSeat(searchSeat);
+            return Ok(result);
+        }
+        [HttpPost]
+        public IActionResult SaveSeat([FromBody]List<SaveSeatModel> saveSeat)
+        {
+            var result = _SeatService.SaveSeat(saveSeat);
             return Ok(result);
         }
     }
--- Avaya.Service/SeatsService/SeatService.cs
+++ Avaya.Service/SeatsService/SeatService.cs
@@ -5,30 +5,34 @@ using System.Text;
 using AutoMapper;
 using Avaya.Core.Extension;
 using Avaya.Core.Repositories;
+using Avaya.Core.UoW;
 using Avaya.Domain.Models;
+using Avaya.Model.SaveSeat;
 using Avaya.Model.ShowSeat;
 
-namespace Avaya.Service.ShowSeatService
+namespace Avaya.Service.SeatService
 {
-    public class ShowSeatService : IShowSeatService
+    public class SeatService : ISeatService
     {
         private readonly IRepository<Booking> _bookingSeatRepository;
         private readonly IRepository<RoomDetail> _roomDetailRepository;
         private readonly IRepository<Room> _roomRepository;
         private readonly IRepository<ShowTime> _showTimeRepository;
         private readonly IRepository<SeatType> _seatTypeRepository;
-
-        public ShowSeatService(IRepository<Booking> bookingSeatRepository,
+        private readonly IUnitOfWork _unitOfWork;
+        public SeatService(IRepository<Booking> bookingSeatRepository,
             IRepository<RoomDetail> roomDetailRepository,
             IRepository<Room> roomRepository,
             IRepository<ShowTime> showTimeRepository,
-            IRepository<SeatType> seatTypeRepository)
+            IRepository<SeatType> seatTypeRepository,
+            IUnitOfWork unitOfWork)
         {
             _bookingSeatRepository = bookingSeatRepository;
             _roomDetailRepository = roomDetailRepository;
             _roomRepository = roomRepository;
             _showTimeRepository = showTimeRepository;
             _seatTypeRepository = seatTypeRepository;
+            _unitOfWork = unitOfWork;
         }
 
         public List<ShowSeatModel> GetShowSeat(SearchSeatModel searchSeat)
@@ -69,6 +73,39 @@ namespace Avaya.Service.ShowSeatService
 
             return null;
 
+        }
+        public bool SaveSeat(List<SaveSeatModel> saveSeatModel)
+        {
+            var roomDetail = _roomDetailRepository.GetAll().ToList();
+            var booking = _bookingSeatRepository.GetAll().ToList();
+
+            foreach (var seat in roomDetail)
+            {
+                foreach (var reservedSeat in saveSeatModel)
+                {
+                    if (reservedSeat.GUID.Contains(seat.Guid.ToString()))
+                    {
+                        foreach (var booked in booking)
+                        {
+                            if (booked.IdShowTime == reservedSeat.ShowTime && booked.IdRoomDetail == seat.Id)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                var insertSeat = AutoMapper.Mapper.Map<Booking>(seat);
+                                insertSeat.Id = 0;
+                                insertSeat.IdRoomDetail = seat.Id;
+                                insertSeat.IdShowTime = reservedSeat.ShowTime;
+                                _bookingSeatRepository.Insert(insertSeat);
+                                _unitOfWork.SaveChanges();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
