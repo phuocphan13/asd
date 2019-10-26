cc330c03479a5918b7be8b1c6dd969c6e0492f33
kimocheese.kimochi@gmail.com <thuanphat0611@gmail.com>
2019-10-26 08:41:39 +0700

    commit

--- Avaya.Domain/Models/BillDetail.cs
+++ Avaya.Domain/Models/BillDetail.cs
@@ -6,11 +6,12 @@ namespace Avaya.Domain.Models
     public partial class BillDetail
     {
         public int Id { get; set; }
-        public int? IdService { get; set; }
         public int? IdBill { get; set; }
+        public int? IdService { get; set; }
         public int? Quantity { get; set; }
         public decimal? Price { get; set; }
 
         public virtual Bill IdBillNavigation { get; set; }
+        public virtual Service IdServiceNavigation { get; set; }
     }
 }
