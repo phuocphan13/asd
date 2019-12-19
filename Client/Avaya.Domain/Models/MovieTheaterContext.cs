17a164383d816686298c7c9e61c50112db732b11
phan phan <manhphuoc1995@gmail.com>
2019-12-19 20:10:08 +0700

    Add Identity Server

--- Client/Avaya.Domain/Models/MovieTheaterContext.cs
+++ Client/Avaya.Domain/Models/MovieTheaterContext.cs
@@ -41,7 +41,7 @@ namespace Avaya.Domain.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=MovieTheater;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-81STRLN\\LUCIFER;Database=MovieTheater;Trusted_Connection=True;");
             }
         }
 
